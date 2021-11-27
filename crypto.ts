f7a4a667252bd2383182afdf720afbd7037a96e2
Anton Kenikh <kenikh@me.com>
2021-11-28 04:02:41 +0500

    Update crypto.ts

--- crypto.ts
+++ crypto.ts
@@ -4,8 +4,8 @@ export default {
   cro1meq5gd2fwjcav8n0nynwelusm9hrngwglvp426: 'Crypto.com',
 
   cro1j3g7qrha6nxh6guvtsz56j06ej8cn3rnyegjz0: 'Crypto.com Migration',
-  cro1w2kvwrzp23aq54n3amwav4yy4a9ahq2kz2wtmj: 'Crypto.com App & Exchange',
-  cro1yjjlx5qsrj5rxn5xtd5rkm6dcqzlchxkrvsmg6: 'Crypto.com App & Exchange',
+  cro1w2kvwrzp23aq54n3amwav4yy4a9ahq2kz2wtmj: 'Crypto.com App',
+  cro1yjjlx5qsrj5rxn5xtd5rkm6dcqzlchxkrvsmg6: 'Crypto.com Exchange',
 
   // used for topup nft accounts
   cro137pc5lte8nadup8su40zte2mt2vw8lxfcmlnxq: 'Crypto.com',
