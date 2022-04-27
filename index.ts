184826a9030ecf02c3111350aeb6169994b5dc5f
Anton Kenikh <kenikh@me.com>
2022-04-27 23:25:05 +0200

    update: OKEx rebrands to OKX

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import bittrex from './bittrex'
 import crypto from './crypto'
 import ibc from './ibc'
-import okex from './okex'
+import okx from './okx'
 
 export default {
   ...bittrex,
   ...crypto,
   ...ibc,
-  ...okex,
+  ...okx,
 }
---
+++ okx.ts
@@ -0,0 +1,3 @@
+export default {
+  cro1ukvuw3qzjtt8wg5hsze4f2c3c8xqvtwgcnxah2: 'OKX.com',
+}
