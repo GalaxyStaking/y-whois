5ee05f6222387774f6802379a13d920484900277
Mr. Kenikh <kenikh@me.com>
2023-04-13 20:56:58 +0000

    Update Cronos module accounts

--- cronos/module.accounts.ts
+++ cronos/module.accounts.ts
@@ -13,12 +13,24 @@ export const moduleAccounts: Record<string, Whois> = {
     label: 'Distribution',
   },
 
+  crc1vqu8rska6swzdmnhf90zuv0xmelej4lq6vlzxk: {
+    label: 'EVM',
+  },
+
   crc17xpfvakm2amg962yls6f84z3kell8c5l88eker: {
     label: 'Fee Collector',
   },
 
+  crc176rcyfn5k9d0wcxel3kmwvxh0hy3xcwewje456: {
+    label: 'Fee IBC',
+  },
+
   crc10d07y265gmmuvt4z0w9aw880jnsr700jdufnyd: {
     label: 'Gov',
+  },
+
+  crc16n3lc7cywa68mg50qhp847034w88pntq3gec8d: {
+    label: 'Gravity',
   },
 
   crc1m3h30wlvsf8llruxtpukdvsy0km2kum8l0aw47: {
