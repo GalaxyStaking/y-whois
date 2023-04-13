495c941262d51341dabf710906829389e0f14c98
Mr. Kenikh <kenikh@me.com>
2023-04-13 20:57:17 +0000

    Update Crypto.org module accounts

--- crypto-org/module.accounts.ts
+++ crypto-org/module.accounts.ts
@@ -13,8 +13,16 @@ export const moduleAccounts: Record<string, Whois> = {
     label: 'Fee Collector',
   },
 
+  cro176rcyfn5k9d0wcxel3kmwvxh0hy3xcwepht664: {
+    label: 'Fee IBC',
+  },
+
   cro10d07y265gmmuvt4z0w9aw880jnsr700jzemu2z: {
     label: 'Gov',
+  },
+
+  cro1vlthgax23ca9syk7xgaz347xmf4nunefln7wsp: {
+    label: 'Interchain Accounts',
   },
 
   cro1m3h30wlvsf8llruxtpukdvsy0km2kum8s20pm3: {
