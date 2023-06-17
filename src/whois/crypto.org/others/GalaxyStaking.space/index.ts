b6dd16e79ee4383e749c51c18e1ce25904ff82cf
Fernando | GalaxyStaking <97257382+fernando-galaxystaking@users.noreply.github.com>
2023-06-17 20:38:27 +0200

    Adding Relayer

---
+++ src/whois/crypto.org/others/GalaxyStaking.space/index.ts
@@ -0,0 +1,8 @@
+import { WhoisRecords } from '../../../../interfaces'
+
+export default <WhoisRecords>{
+  cro1lwymrvh7vpwzqfkgpvjtz2y5gqelw4m7tq0w3e: {
+    label: 'GalaxyStaking.space Relayer',
+    link: 'https://galaxystaking.space',
+  },
+}
