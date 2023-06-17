2eb9700ebdf3d4d1b026de8c95867f6d2b75cb45
Fernando | GalaxyStaking <97257382+fernando-galaxystaking@users.noreply.github.com>
2023-06-17 20:41:45 +0200

    Adding Restakes

--- src/whois/crypto.org/others/GalaxyStaking.space/index.ts
+++ src/whois/crypto.org/others/GalaxyStaking.space/index.ts
@@ -5,4 +5,16 @@ export default <WhoisRecords>{
     label: 'GalaxyStaking.space Relayer',
     link: 'https://galaxystaking.space',
   },
+  cro1fvedw32vg68kv83s6zzgh4p06gydvf78t2z0cr: {
+    label: 'GalaxyStaking.space - Neptune Restake',
+    link: 'https://galaxystaking.space',
+  },
+  cro19ewjs3w6r0frmgljhdts70hz9wlcyrfx2wed27: {
+    label: 'GalaxyStaking.space - Saturn Restake',
+    link: 'https://galaxystaking.space',
+  },
+  cro1lemc6jwfny6ryjaf37w62a6xdpttrr0t0var3n: {
+    label: 'GalaxyStaking.space - Uranus Restake',
+    link: 'https://galaxystaking.space',
+  }
 }
