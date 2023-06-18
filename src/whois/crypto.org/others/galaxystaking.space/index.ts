f4abd65e51923fcfaf0d74c2c169ab581be39c82
Fernando | GalaxyStaking <97257382+fernando-galaxystaking@users.noreply.github.com>
2023-06-18 14:19:57 +0200

    Correction :D

---
+++ src/whois/crypto.org/others/galaxystaking.space/index.ts
@@ -0,0 +1,20 @@
+import { WhoisRecords } from '../../../../interfaces'
+
+export default <WhoisRecords>{
+  cro1lwymrvh7vpwzqfkgpvjtz2y5gqelw4m7tq0w3e: {
+    label: 'GalaxyStaking Relayer',
+    link: 'https://galaxystaking.space',
+  },
+  cro1fvedw32vg68kv83s6zzgh4p06gydvf78t2z0cr: {
+    label: 'GalaxyStaking - Neptune Restake',
+    link: 'https://galaxystaking.space',
+  },
+  cro19ewjs3w6r0frmgljhdts70hz9wlcyrfx2wed27: {
+    label: 'GalaxyStaking - Saturn Restake',
+    link: 'https://galaxystaking.space',
+  },
+  cro1lemc6jwfny6ryjaf37w62a6xdpttrr0t0var3n: {
+    label: 'GalaxyStaking - Uranus Restake',
+    link: 'https://galaxystaking.space',
+  }
+}
